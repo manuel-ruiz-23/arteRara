037b7165b87968c77dbdd690db0eecf763d49277
manuel-ruiz-23 <rrmanuel1123@gmail.com>
2020-02-07 21:32:09 -0700

    paint pro

--- paintPro/app.js
+++ paintPro/app.js
@@ -22,22 +22,30 @@ var pos = { x: 0, y: 0 };
 
 // new position from mouse events
 function setPosition(e) {
-  pos.x = e.clientX-15;
+  pos.x = e.clientX-7;
   // pos.x = e.clientX;
-  pos.y = e.clientY-30;
+  pos.y = e.clientY-42;
   // pos.y = e.clientY;
+}
+
+let color = '#000000';
+function setColor(newcolor) {
+  // console.log(getComputedStyle(e.target).backgroundColor);
+  console.log(newcolor);
+  color = newcolor;
 }
 
 function draw(e) {
   if (e.buttons !== 1) return; // if mouse is pressed.....
 
-  var color = document.getElementById("hex").value;
+  var tamaño = document.getElementById("hex").value;
 
   ctx.beginPath(); // begin the drawing path
 
-  ctx.lineWidth = 20; // width of lineS
+  ctx.lineWidth = Number(tamaño); // width of lineS
   ctx.lineCap = "round"; // rounded end cap
   ctx.strokeStyle = color; // hex color of line
+  // ctx.strokeStyle = "#4e0f70"; // hex color of line
 
   ctx.moveTo(pos.x, pos.y); // from position
   setPosition(e);
